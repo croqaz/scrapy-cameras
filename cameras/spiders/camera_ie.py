1762b847e44855c5ed1fb9f75dffbd258caae167
Cristi Constantin <cristi.constantin@sent.com>
2019-02-13 21:11:48 +0000

    Fixed camera.ie spider

--- cameras/spiders/camera_ie.py
+++ cameras/spiders/camera_ie.py
@@ -26,9 +26,9 @@ class CameraIeSpider(scrapy.Spider):
         name = extract_text(item.css('.child_title').get(), guess_layout=False)
         price = item.css('.centered .styleColor::text').get()
         price = price.split('€')[-1].strip().replace(',', '')
-        uid = item.xpath('//div[@class="item-box-desc"]/p/i[contains(text(), "Product Code")]/text()')
+        uid = item.xpath('.//div[@class="item-box-desc"]/p/i[contains(text(), "Product Code")]/text()')
         uid = uid.get().split(':')[-1].strip()
-        ean = item.xpath('//div[@class="item-box-desc"]/p/i[contains(text(), "Barcode/EAN")]/text()')
+        ean = item.xpath('.//div[@class="item-box-desc"]/p/i[contains(text(), "Barcode/EAN")]/text()')
         ean = ean.get().split(':')[-1].strip()
         return {
             'T': utc_time(),
